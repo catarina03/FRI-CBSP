28cf072479f6f985fe67654dcf76f24da2436007
Catarina Fernandes <up201806610@fe.up.pt>
2022-11-11 10:52:22 +0100

    Started lab 6

--- 06midi/src/main/java/cbsp/MIDIsynthesizer.java
+++ 06midi/src/main/java/cbsp/MIDIsynthesizer.java
@@ -1,5 +1,10 @@
 package cbsp;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.ShortMessage;
+
 public class MIDIsynthesizer {
 	
 	// TODO Create classes for MIDI messages for Task 4 in 6.
@@ -10,8 +15,48 @@ public class MIDIsynthesizer {
 
 	}
 
+	protected MidiMessage createMidiNoteOnMessage(int status, int pitch, int velocity){
+
+	}
+
+	protected MidiMessage createMidiNoteOffMessage(int status, int pitch, int velocity){
+
+	}
+
+	protected MidiMessage createMidiProgramChangeMessage(int instrument){
+		ShortMessage msg = new ShortMessage();
+		try {
+			msg.setMessage(ShortMessage.PROGRAM_CHANGE, 0, instrument, 0);
+		}
+		catch (InvalidMidiDataException e){
+			throw new InvalidMidiDataException(e);
+		}
+	}
+
 	public static void main(String[] args) {
-		
+		try {
+
+
+			synthesizer.open();
+
+			MidiMessage noteOn = createMidiNoteOnMessage(60, 60);
+			MidiMessage noteOff = createMidiNoteOffMessage(60, 30);
+			MidiMessage programChange = createMidiProgramChangeMessage(100);
+
+			rcv.send(programChange, 0);
+			rcv.send(noteOn, 0);
+			Thread.sleep(1000);
+			rcv,.send(noteOff, 0);
+
+			MidiChannel channel = synth.getChannels();
+			channel.noteOn(70,40);
+			Thread.sleep(1000);
+			channel.noteOff(70, 50);
+
+		} catch (  InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
 		MIDIsynthesizer synthesizer = new MIDIsynthesizer();
 			
 	}
