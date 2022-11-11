4e3df7ce11f38236b34d412f8ac28f35a34ffad8
Catarina Fernandes <up201806610@fe.up.pt>
2022-11-11 10:39:24 +0100

    Add lab 6

---
+++ 06midi/src/main/java/cbsp/MIDIsynthesizer.java
@@ -0,0 +1,19 @@
+package cbsp;
+
+public class MIDIsynthesizer {
+	
+	// TODO Create classes for MIDI messages for Task 4 in 6.
+	
+	public MIDIsynthesizer() {
+		
+		// TODO Task 1 - 9. Be mindful of handling exceptions.
+
+	}
+
+	public static void main(String[] args) {
+		
+		MIDIsynthesizer synthesizer = new MIDIsynthesizer();
+			
+	}
+
+}
